10026fa2482af8464c122f2c51a25a68cf42b251
Paul Le Bolloch <paul.lebolloch@student-cs.fr>
2025-04-06 21:48:13 +0200

    first perceive deliberate act impl. with no collision/teleportation

--- agents.py
+++ agents.py
@@ -1,26 +1,69 @@
+#agents.py
+
 import mesa
+from objects import WasteDisposalZone, RadioactivityAgent, Waste
+
 
 class MyAgent(mesa.Agent):
-    """An agent with fixed initial wealth."""
+    """An agent with fixed initial knowledge."""
 
     def __init__(self, model):
-        """initialize a MoneyAgent instance.
-
-        Args:
-            model: A model instance
-        """
         super().__init__(model)
+        self.knowledge = {"grid_size": (self.model.grid.width, self.model.grid.height),
+            "position": self.pos,
+            "possible_steps": [],
+            "neighbor_waste_disposal_zone": [],
+            "neighbor_radioactivity_agent": [],
+            "neighbor_waste": [],
+            "neighbor_robot": [],
+            "neighbors": []}
+        self.next_action = []
 
-    def move(self):
-        """move to a random neighboring cell."""
-        possible_steps = self.model.grid.get_neighborhood(
-            self.pos, moore=True, include_center=False
-        )
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
+    def perceive(self, percepts):
+        """Update agent's perception of its environment using Von Neumann neighborhood."""
+        self.knowledge["neighbors"] = percepts
+
+        # Reset memory
+        self.knowledge["possible_steps"] = []
+        self.knowledge["neighbor_waste_disposal_zone"] = []
+        self.knowledge["neighbor_radioactivity_agent"] = []
+        self.knowledge["neighbor_waste"] = []
+        self.knowledge["neighbor_robot"] = []
+
+        for agent in self.knowledge["neighbors"]:
+            if isinstance(agent, WasteDisposalZone):
+                self.knowledge["neighbor_waste_disposal_zone"].append([agent, agent.pos, agent.radioactivity])
+            if isinstance(agent, RadioactivityAgent):
+                self.knowledge["neighbor_radioactivity_agent"].append([agent, agent.pos, agent.radioactivity])
+            if isinstance(agent, Waste):
+                self.knowledge["neighbor_waste"].append([agent, agent.color])
+            if isinstance(agent, MyAgent):
+                self.knowledge["neighbor_robot"].append([agent, agent.pos])
 
 
+    def deliberate(self):
+
+        # Determine possible steps (only cells that are not occupied by other MyAgent)
+        impossible_steps = []
+        for agent in self.knowledge["neighbor_robot"]:
+            impossible_steps.append(agent[1])
+        
+        for position in self.model.grid.get_neighborhood(self.pos, moore=False):
+            if position not in impossible_steps:
+                self.knowledge["possible_steps"].append(position)
+
+        self.next_action.append("move_random")
+
+
+    def do(self):
+        """Execute the decided action."""
+        return self.next_action[-1]
+        
     def step(self):
-        """do one step of the agent."""
-        self.move()
+        """Execute the agent's step."""
+        
+        self.deliberate()
+        action = self.do()
+        percepts = self.model.do(self, action)
+        self.perceive(percepts)
 
--- objects.py
+++ objects.py
@@ -1,9 +1,9 @@
-#%%
+#objects.py
+
 import numpy as np
 import random as rd
 import mesa
 
-#%%
 class RadioactivityAgent(mesa.Agent):
     def __init__ (self,model,zone):
         super().__init__(model)
@@ -17,15 +17,14 @@ class RadioactivityAgent(mesa.Agent):
             return rd.uniform(0.33,0.66)
         else:
             return rd.uniform(0,0.33)
-# %%
+
 class WasteDisposalZone(RadioactivityAgent):
     def __init__(self,model,zone):
         super().__init__(model,zone)
         self.radioactivity = -1
-# %%
+
 class Waste(mesa.Agent):
     def __init__(self, model, color):
         super().__init__(model) 
         self.color = color
 
-# %%
